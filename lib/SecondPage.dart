eb2ffeb19787a193074ae39f7f24ea07c8390d96
ZhiruXie <danielxie9735@gmail.com>
2025-06-02 23:19:49 -0400

    introduce encrypted preference and add listener to TextFields for recording user input values dynamically

--- lib/SecondPage.dart
+++ lib/SecondPage.dart
@@ -1,3 +1,4 @@
+import 'package:encrypted_shared_preferences/encrypted_shared_preferences.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:my_cst2335_labs/DataRepository.dart';
@@ -10,7 +11,12 @@ class SecondPage extends StatefulWidget {
 }
 
 class _SecondPageState extends State<SecondPage> {
+  late EncryptedSharedPreferences ePrefs;
   late String loginName;
+  final TextEditingController _firstNameController = TextEditingController();
+  final TextEditingController _lastNameController = TextEditingController();
+  final TextEditingController _phoneController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
 
   @override //same as in java
   void initState(){
@@ -19,7 +25,17 @@ class _SecondPageState extends State<SecondPage> {
   }
 
   void _initializeApp() async {
+    ePrefs = EncryptedSharedPreferences();
     loginName = DataRepository.loginName;
+  }
+
+  @override
+  void dispose() {
+    _firstNameController.dispose();
+    _lastNameController.dispose();
+    _phoneController.dispose();
+    _emailController.dispose();
+    super.dispose();
   }
 
   @override
@@ -33,61 +49,79 @@ class _SecondPageState extends State<SecondPage> {
       // the App.build method, and use it to set our appbar title.
     ), body
         :Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      spacing: 15,
-      children: <Widget>[
-        Text(style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),"Welcome Back $loginName"),
-        TextField(
+        mainAxisAlignment: MainAxisAlignment.start,
+        spacing: 15,
+        children: <Widget>[
+          Text(style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),"Welcome Back $loginName"),
+          TextField(
+            controller: _firstNameController,
             decoration: const InputDecoration(
-            labelText:"First Name",
-            border: OutlineInputBorder()
+              labelText:"First Name",
+              border: OutlineInputBorder()
+            ),
+            onChanged: (value) async {
+              await ePrefs.setString("firstName", value);
+            }
           ),
-        ),
-        TextField(
+          TextField(
+            controller: _lastNameController,
             decoration: const InputDecoration(
-            labelText:"Last Name",
-            border: OutlineInputBorder()
+              labelText:"Last Name",
+              border: OutlineInputBorder()
+            ),
+            onChanged: (value) async {
+              await ePrefs.setString("lastName", value);
+            }
           ),
-        ),
-        Row(
-          children: <Widget>[
-            Flexible(
-              child: TextField(
-                decoration: const InputDecoration(
-                labelText: "Phone Number",
-                border: OutlineInputBorder()
-            ))),
-            IconButton(
-              icon: const Icon(Icons.phone),
-              onPressed: () {
-                // Handle phone icon tap if needed
-              },
-            ),
-            IconButton(
-              icon: const Icon(Icons.message),
-              onPressed: () {
-                // Handle phone icon tap if needed
-              },
-            ),
-          ],
-        ),
-        Row(
-          children: <Widget>[
-            Flexible(
-                child: TextField(
-                  decoration: const InputDecoration(
-                  labelText: "Email Address",
-                  border: OutlineInputBorder()
-            ))),
-            IconButton(
-              icon: const Icon(Icons.mail),
-              onPressed: () {
-                // Handle phone icon tap if needed
-              },
-            )
-          ],
-        )
-      ])
+          Row(
+            children: <Widget>[
+              Flexible(
+                  child: TextField(
+                    controller: _phoneController,
+                    decoration: const InputDecoration(
+                      labelText: "Phone Number",
+                      border: OutlineInputBorder()
+                    ),
+                    onChanged: (value) async {
+                      await ePrefs.setString("phone", value);
+                    }
+                  )),
+              IconButton(
+                icon: const Icon(Icons.phone),
+                onPressed: () {
+                  // Handle phone icon tap if needed
+                },
+              ),
+              IconButton(
+                icon: const Icon(Icons.message),
+                onPressed: () {
+                  // Handle phone icon tap if needed
+                },
+              ),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Flexible(
+                  child: TextField(
+                    controller: _emailController,
+                    decoration: const InputDecoration(
+                      labelText: "Email Address",
+                      border: OutlineInputBorder()
+                    ),
+                    onChanged: (value) async {
+                      await ePrefs.setString("email", value);
+                    }
+                  )),
+              IconButton(
+                icon: const Icon(Icons.mail),
+                onPressed: () {
+                  // Handle phone icon tap if needed
+                },
+              )
+            ],
+          )
+        ])
     ); //Use a Scaffold to layout a page with an AppBar and main body region
   }
 }
